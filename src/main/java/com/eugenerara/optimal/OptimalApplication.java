dd40c7ce83aaac4566e47543a3d6dac2aa53f156
Eugene Tan Qi Yuan <eugene_tanqiyuan@hotmail.com>
2024-08-26 23:52:20 +0800

    Initial commit

---
+++ src/main/java/com/eugenerara/optimal/OptimalApplication.java
@@ -0,0 +1,13 @@
+package com.eugenerara.optimal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptimalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptimalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eugenerara/optimal/OptimalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eugenerara.optimal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptimalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
