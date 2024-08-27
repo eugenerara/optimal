002567d8c0abacfb72f0c8e4612553508919f493
eugenerara <eugene_tanqiyuan@hotmail.com>
2024-08-27 22:27:03 +0800

    implement service

---
+++ src/main/java/com/eugenerara/optimal/encryption/AESEncryptionServiceImpl.java
@@ -0,0 +1,9 @@
+package com.eugenerara.optimal.encryption;
+
+public class AESEncryptionServiceImpl implements EncryptionService {
+  @Override
+  public void encrypt(String inputFilePath, String outputFilePath) {}
+
+  @Override
+  public void decrypt(String inputFilePath, String outputFilePath) {}
+}
